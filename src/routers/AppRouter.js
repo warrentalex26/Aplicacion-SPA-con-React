83e4d56624b6ef6771ffdf97948561a4dbb2dd78
warrentalex26 <warrentalex26ies@gmail.com>
2021-03-04 14:25:26 -0600

    Rutas privadas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
     Route,
 } from "react-router-dom"
+import { AuthContext } from '../auth/AuthContext'
 import { LoginScreen } from '../components/login/LoginScreen'
 import { Navbar } from '../components/ui/Navbar'
 import { DashboardRoutes } from './DashboardRoutes'
+import { PrivateRoute } from './PrivateRoute'
 
 export const AppRouter = () => {
+
+
+    const {user} = useContext(AuthContext);
+    console.log(user)
 
     return (
         <Router>
@@ -16,7 +22,7 @@ export const AppRouter = () => {
                 {/* <Navbar/> */}
                 <Switch>
                     <Route exact path='/login' component={LoginScreen}></Route>
-                    <Route path='/' component={DashboardRoutes}></Route>
+                    <PrivateRoute path='/' component={DashboardRoutes} isAuthenticated={user.logged}/>
                 </Switch>
             </div>
         </Router>
